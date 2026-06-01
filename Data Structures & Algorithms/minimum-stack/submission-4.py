45712e6385f03600185a7787e40129f3ab7374fd
Vincent Nguyen <68995370+TheVincinator@users.noreply.github.com>
2026-06-01 11:37:15 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.prefix = []
+        self.minimum = float('inf')
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.prefix.append(min(self.minimum, val))
+        self.minimum = min(self.minimum, val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.prefix.pop()
+        if self.prefix:
+            self.minimum = self.prefix[-1]
+        else:
+            self.minimum = float('inf')
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.prefix[-1]
