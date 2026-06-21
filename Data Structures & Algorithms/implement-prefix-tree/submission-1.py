ba671d7812d1e81f8d483ba93c9fa3eda92bc583
Vincent Nguyen <68995370+TheVincinator@users.noreply.github.com>
2026-06-22 00:57:30 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode() 
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for i, char in enumerate(word):
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+            if i == len(word) - 1:
+                curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for i, char in enumerate(word):
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+            if i == len(word) - 1:
+                if not curr.endOfWord:
+                    return False
+        return True
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return True
+        
+        
+        
