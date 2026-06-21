fc1547ca6b4a4e25c69aa9dc0369a095596f2cfd
Vincent Nguyen <68995370+TheVincinator@users.noreply.github.com>
2026-06-22 02:39:36 +0530

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,32 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        def searchRec(word, curr):
+            for i, c in enumerate(word):
+                if c == ".":
+                    for k in curr.children:
+                        if not searchRec(word[i + 1:], curr.children[k]):
+                            continue
+                        return True
+                    return False
+                elif c not in curr.children:
+                    return False
+                curr = curr.children[c]
+            return curr.endOfWord
+        return searchRec(word, self.root)
