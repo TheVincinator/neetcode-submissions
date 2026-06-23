49c35fd58370bc945e9b71cdfe2a200aa0691dbe
Vincent Nguyen <68995370+TheVincinator@users.noreply.github.com>
2026-06-24 00:48:53 +0530

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        visited = set()
+        fresh = 0
+        queue = deque()
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        def bfs():
+            minTime = 0
+            rotted = 0
+            while queue:
+                r, c, time = queue.popleft()
+                minTime = max(minTime, time)
+                for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    if (r + dr in range(ROWS) and
+                        c + dc in range(COLS) and
+                        (r + dr, c + dc) not in visited and
+                        grid[r + dr][c + dc] != 0):
+                        if grid[r + dr][c + dc] == 1:
+                            rotted += 1
+                        queue.append((r + dr, c + dc, time + 1))
+                        visited.add((r + dr, c + dc))
+            return -1 if fresh != rotted else minTime
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 2:
+                    queue.append((i, j, 0))
+                    visited.add((i, j))
+                elif grid[i][j] == 1:
+                    fresh += 1
+
+        return bfs()
