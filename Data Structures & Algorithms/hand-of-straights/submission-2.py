e5922f99818916d1e5d48c1f7acaa2c4dbaeaffa
Vincent Nguyen <68995370+TheVincinator@users.noreply.github.com>
2026-07-28 01:09:58 +0530

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        # [1,2,2,3,3,4,4,5]
+        if len(hand) % groupSize != 0:
+            return False
+
+        minHeap = []
+        handMap = defaultdict(int)
+        for card in hand:
+            handMap[card] += 1
+        for key in handMap:
+            heapq.heappush(minHeap, key)
+
+        start = minHeap[0]
+        size = 0
+        while handMap:
+            if size == groupSize:
+                start = minHeap[0]
+                size = 0
+            if start not in handMap:
+                return False
+            handMap[start] -= 1
+            if not handMap[start]:
+                if start != minHeap[0]:
+                    return False
+                heapq.heappop(minHeap)
+                del handMap[start]
+            size += 1
+            start += 1
+        return True
