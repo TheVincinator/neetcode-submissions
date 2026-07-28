74325c68dc20d4f2f40bb8fdd4a405d876aa5fc4
Vincent Nguyen <68995370+TheVincinator@users.noreply.github.com>
2026-07-28 21:33:26 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        freqs = defaultdict(int)
+        for c in s:
+            freqs[c] += 1
+        
+        lastIndices = defaultdict(int)
+        for i, c in enumerate(s):
+            freqs[c] -= 1
+            if not freqs[c]:
+                lastIndices[c] = i
+
+        res = []
+        i = 0
+        j = 0
+        lastIndex = 0
+        while j < len(s):
+            lastIndex = max(lastIndex, lastIndices[s[j]])
+            if j == lastIndex:
+                res.append(j - i + 1)
+                i = j + 1
+            j += 1
+        
+        return res
+
+
+
+        
